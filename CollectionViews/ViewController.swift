4d9375e9372c86f2d0800749cc030d3deca1738b
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-01-02 13:47:37 -0500

    Initial Commit

---
+++ CollectionViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViews
+//
+//  Created by Oscar Victoria Gonzalez  on 1/2/20.
+//  Copyright © 2020 Oscar Victoria Gonzalez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
