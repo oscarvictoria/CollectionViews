48799183f09f8503671c007ef1fd19413f10cdf6
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-01-07 00:08:54 -0500

    updated

---
+++ CollectionViews/CollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionCellCollectionViewCell.swift
+//  CollectionViews
+//
+//  Created by Oscar Victoria Gonzalez  on 1/6/20.
+//  Copyright © 2020 Oscar Victoria Gonzalez . All rights reserved.
+//
+
+import UIKit
+
+class CollectionCell: UICollectionViewCell {
+    
+@IBOutlet weak var nameLabel: UILabel!
+    
+    
+    
+}
--- CollectionViews/ViewController.swift
+++ CollectionViews/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
 @IBOutlet weak var collectionView: UICollectionView!
     
+    let names = ["Anderson", "Ashwoon", "Aikin", "Bateman", "Bongard", "Bowers", "Boyd", "Cannon", "Cast", "Deitz", "Dewalt", "Ebner", "Frick", "Hancock", "Haworth", "Hesch", "Hoffman", "Kassing", "Knutson", "Lawless", "Lawicki", "Mccord", "McCormack", "Miller", "Myers", "Nugent", "Ortiz", "Orwig", "Ory", "Paiser", "Pak", "Pettigrew", "Quinn", "Quizoz", "Ramachandran", "Resnick", "Sagar", "Schickowski", "Schiebel", "Sellon", "Severson", "Shaffer", "Solberg", "Soloman", "Sonderling", "Soukup", "Soulis", "Stahl", "Sweeney", "Tandy", "Trebil", "Trusela", "Trussel", "Turco", "Uddin", "Uflan", "Ulrich", "Upson", "Vader", "Vail", "Valente", "Van Zandt", "Vanderpoel", "Ventotla", "Vogal", "Wagle", "Wagner", "Wakefield", "Weinstein", "Weiss", "Woo", "Yang", "Yates", "Yocum", "Zeaser", "Zeller", "Ziegler", "Bauer", "Baxster", "Casal", "Cataldi", "Caswell", "Celedon", "Chambers", "Chapman", "Christensen", "Darnell", "Davidson", "Davis", "DeLorenzo", "Dinkins", "Doran", "Dugelman", "Dugan", "Duffman", "Eastman", "Ferro", "Ferry", "Fletcher", "Fietzer", "Hylan", "Hydinger", "Illingsworth", "Ingram", "Irwin", "Jagtap", "Jenson", "Johnson", "Johnsen", "Jones", "Jurgenson", "Kalleg", "Kaskel", "Keller", "Leisinger", "LePage", "Lewis", "Linde", "Lulloff", "Maki", "Martin", "McGinnis", "Mills", "Moody", "Moore", "Napier", "Nelson", "Norquist", "Nuttle", "Olson", "Ostrander", "Reamer", "Reardon", "Reyes", "Rice", "Ripka", "Roberts", "Rogers", "Root", "Sandstrom", "Sawyer", "Schlicht", "Schmitt", "Schwager", "Schutz", "Schuster", "Tapia", "Thompson", "Tiernan", "Tisler"]
+
     
 
     override func viewDidLoad() {
@@ -43,11 +45,15 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return names.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "nameCell", for: indexPath)
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "nameCell", for: indexPath) as? CollectionCell else {
+            fatalError("Could not get cell")
+        }
+        let theNames = names[indexPath.row]
+        cell.nameLabel.text = theNames
         return cell
     }
 }
